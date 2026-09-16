da13450c6057af568a83c451058e93e35660b00c
edibrata <edibratabanten@gmail.com>
2026-09-16 18:17:42 +0700

    build: update base path to /E-Brata/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/dummyrapor/',
+    base: '/E-Brata/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
